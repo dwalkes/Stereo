2222bfbcc61c4425986b361b7277f88c9a3cb3c1
Daiver <ra22341@ya.ru>
2013-05-14 22:17:28 +0400

    full ver of stereovar tuner

--- StereoBMTuner/stereoalltune.cpp
+++ StereoBMTuner/stereoalltune.cpp
@@ -19,6 +19,10 @@ int var_nIt = 25;
 int var_minDisp;
 int var_tmp_poly_n = 9;
 int var_poly_n = 9;
+int var_maxDisp = 0;
+int var_polySigma = 17;
+int var_fi = 30;
+int var_lambda = 4;
 
 cv::Mat getDepthMapVar(cv::Mat const &left, cv::Mat const &right)
 {
@@ -27,13 +31,14 @@ cv::Mat getDepthMapVar(cv::Mat const &left, cv::Mat const &right)
     var.levels = 3; // ignored with USE_AUTO_PARAMS (!)
     var.pyrScale = 0.5;                             // ignored with USE_AUTO_PARAMS
     var.nIt = var_nIt; //25;
-    var.minDisp = var_minDisp; //    var.maxDisp = 0;
+    var.minDisp = var_minDisp; //    
+    var.maxDisp = var_maxDisp; //0;
     //var.poly_n = 3;
     var.poly_n = var_poly_n; //9;
     //var.poly_sigma = 0.0;
-    var.poly_sigma = 1.7;
-    var.fi = 15.0f;//nice
-    var.lambda = 0.04f;
+    var.poly_sigma = var_polySigma * 0.1; //1.7;
+    var.fi =  var_fi/2.0;//15.0f;//nice
+    var.lambda = var_lambda*0.01;//0.04f;
     var.penalization = var.PENALIZATION_TICHONOV;   // ignored with USE_AUTO_PARAMS
     var.cycle = var.CYCLE_V;                        // ignored with USE_AUTO_PARAMS
     var.flags = var.USE_SMART_ID | var.USE_AUTO_PARAMS | var.USE_INITIAL_DISPARITY | var.USE_MEDIAN_FILTERING ;
@@ -92,6 +97,10 @@ int main(int argc, char** argv)
     cv::imshow("depth map", dm);
     cv::createTrackbar( "nIt", "depth map", &var_nIt, 200, on_trackbar );
     cv::createTrackbar( "mindisp", "depth map", &var_minDisp, 200, on_trackbar );
+    cv::createTrackbar( "maxDisp", "depth map", &var_maxDisp, 200, on_trackbar );
     cv::createTrackbar( "poly_n", "depth map", &var_tmp_poly_n, 20, on_trackbar );
+    cv::createTrackbar( "poly_sigma", "depth map", &var_polySigma, 100, on_trackbar );
+    cv::createTrackbar( "fi", "depth map", &var_fi, 100, on_trackbar );
+    cv::createTrackbar( "lambda", "depth map", &var_lambda, 100, on_trackbar );
     cv::waitKey(0);
 }
