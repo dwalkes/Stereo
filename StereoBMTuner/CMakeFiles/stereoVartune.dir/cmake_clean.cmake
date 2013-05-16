8a13f4a8483ec8a02a8614feb15637994d8d0855
Daiver <ra22341@ya.ru>
2013-05-16 17:49:36 +0400

    switch tune to cmake

---
+++ StereoBMTuner/CMakeFiles/stereoVartune.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/stereoVartune.dir/stereoVartune.cpp.o"
+  "stereoVartune.pdb"
+  "stereoVartune"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/stereoVartune.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
