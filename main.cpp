551a0c0789f8b5d296227c9e92f9044445bebac4
Daiver <ra22341@ya.ru>
2013-05-18 10:27:05 +0400

    split clusters filtering into function

--- main.cpp
+++ main.cpp
@@ -51,6 +51,18 @@ pcl::RegionGrowing<pcl::PointXYZRGB, pcl::Normal> getColored(pcl::PointCloud<pcl
     return reg;
 }
 
+std::vector <pcl::PointIndices> getFilteredClusters(pcl::RegionGrowing<pcl::PointXYZRGB, pcl::Normal>& reg)
+{
+    std::vector <pcl::PointIndices> clusters, tmp_clusters;
+    reg.extract (tmp_clusters);
+    for(int i = 0; i < tmp_clusters.size(); i++)
+    {
+        if(tmp_clusters[i].indices.size() > 0)
+        clusters.push_back(tmp_clusters[i]);
+    }
+    return clusters;
+}
+
 //This function creates a PCL visualizer, sets the point cloud to view and returns a pointer
 boost::shared_ptr<pcl::visualization::PCLVisualizer> createVisualizer (pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr cloud)
 {
@@ -63,87 +75,6 @@ boost::shared_ptr<pcl::visualization::PCLVisualizer> createVisualizer (pcl::Poin
     viewer->initCameraParameters ();
     return (viewer);
 }
-
-/*
-void straight_reproject_cloud(cv::Mat& img_rgb, cv::Mat& img_disparity, pcl::PointCloud<pcl::PointXYZRGB>::Ptr& point_cloud_ptr)
-{
-    uchar pr, pg, pb;
-    for (int i = 0; i < img_rgb.rows; i++)
-    {
-        uchar* rgb_ptr = img_rgb.ptr<uchar>(i);
-        uchar* disp_ptr = img_disparity.ptr<uchar>(i);
-        for (int j = 0; j < img_rgb.cols; j++)
-        {
-            uchar d = disp_ptr[j];
-            if ( d == 0 ) continue; //Discard bad pixels
-            pb = rgb_ptr[3*j];
-            pg = rgb_ptr[3*j+1];
-            pr = rgb_ptr[3*j+2];
-            //Insert info into point cloud structure
-            pcl::PointXYZRGB point;
-            point.x = j;
-            point.y = i;
-            point.z = d;
-            uint32_t rgb = (static_cast<uint32_t>(pr) << 16 |
-                  static_cast<uint32_t>(pg) << 8 | static_cast<uint32_t>(pb));
-            point.rgb = *reinterpret_cast<float*>(&rgb);
-            point_cloud_ptr->push_back (point);
-        } 
-    }
-}
-
-void complex_reproject_cloud(const cv::Mat& Q, cv::Mat& img_rgb, cv::Mat& img_disparity, pcl::PointCloud<pcl::PointXYZRGB>::Ptr& point_cloud_ptr)
-{
-    //Get the interesting parameters from Q
-    double Q03, Q13, Q23, Q32, Q33;
-    Q03 = Q.at<double>(0,3);
-    Q13 = Q.at<double>(1,3);
-    Q23 = Q.at<double>(2,3);
-    Q32 = Q.at<double>(3,2);
-    Q33 = Q.at<double>(3,3);
-
-    std::cout << "Q(0,3) = "<< Q03 <<"; Q(1,3) = "<< Q13 <<"; Q(2,3) = "<< Q23 <<"; Q(3,2) = "<< Q32 <<"; Q(3,3) = "<< Q33 <<";" << std::endl;
-
-    double px, py, pz;
-    uchar pr, pg, pb;
-
-    for (int i = 0; i < img_rgb.rows; i++)
-    {
-        uchar* rgb_ptr = img_rgb.ptr<uchar>(i);
-        uchar* disp_ptr = img_disparity.ptr<uchar>(i);
-        for (int j = 0; j < img_rgb.cols; j++)
-        {
-            //Get 3D coordinates
-            uchar d = disp_ptr[j];
-            if ( d == 0 ) continue; //Discard bad pixels
-            double pw = -1.0 * static_cast<double>(d) * Q32 + Q33; 
-            px = static_cast<double>(j) + Q03;
-            py = static_cast<double>(i) + Q13;
-            pz = Q23;
-
-            px = px/pw;
-            py = py/pw;
-            pz = pz/pw;
-            //Get RGB info
-            pb = rgb_ptr[3*j];
-            pg = rgb_ptr[3*j+1];
-            pr = rgb_ptr[3*j+2];
-            //Insert info into point cloud structure
-            pcl::PointXYZRGB point;
-            point.x = px;
-            point.y = py;
-            point.z = pz;
-            uint32_t rgb = (static_cast<uint32_t>(pr) << 16 |
-                  static_cast<uint32_t>(pg) << 8 | static_cast<uint32_t>(pb));
-            point.rgb = *reinterpret_cast<float*>(&rgb);
-            point_cloud_ptr->push_back (point);
-        }
-    }
-    point_cloud_ptr->width = (int) point_cloud_ptr->points.size();
-    point_cloud_ptr->height = 1;
-
-}
-*/
 
 int main( int argc, char** argv )
 {
@@ -200,51 +131,15 @@ int main( int argc, char** argv )
     auto reg = getColored(point_cloud_ptr);
     end = clock();
     std::cout <<"time elapsed"<< (double)(end - begin) / CLOCKS_PER_SEC <<std::endl;
-
-    std::vector <pcl::PointIndices> clusters, tmp_clusters;
-    reg.extract (tmp_clusters);
-    for(int i = 0; i < tmp_clusters.size(); i++)
-    {
-        if(tmp_clusters[i].indices.size() > 0)
-        clusters.push_back(tmp_clusters[i]);
-    }
-
+    auto clusters = getFilteredClusters(reg);
     std::cout << "Number of clusters is equal to " << clusters.size () << std::endl;
     std::cout << "First cluster has " << clusters[0].indices.size () << " points." << endl;
     std::cout << "These are the indices of the points of the initial" <<
     std::endl << "cloud that belong to the first cluster:" << std::endl;
-  //384 288
-  int counter = 0;
-  /*for(int j = 0; j < clusters.size(); j++)
-  {
-      cv::Mat to_show = cv::Mat::zeros(288, 384, cv::DataType<uchar>::type);
-      std::cout <<j << " " << clusters[j].indices.size() << std::endl;
-      for(int i = 0; i < clusters[j].indices.size(); i++)
-      {
-        //std::cout << clusters[j].indices[i] << std::endl;
-        //std::cout << cloud->at(clusters[j].indices[i])<< std:: endl;
-        to_show.data[clusters[j].indices[i]] = 200;
-        //to_show.at<uchar>(clusters[j].indices[i]/cloud->height, clusters[j].indices[i]%cloud->height) = 200;
-        //to_show.at<bool>((int)(cloud->at(clusters[j].indices[i]).y), (int)(cloud->at(clusters[j].indices[i]).x)) = 200;
-        counter++;
-      }
-      cv::imshow("", to_show);
-      cv::waitKey();
-  }*/
-  /*cv::Mat to_show = cv::Mat::zeros(288, 384, cv::DataType<uchar>::type);
-  for(int i = 0; i < point_cloud_ptr->height; i++)
-  {
-    for(int j = 0; j < point_cloud_ptr->width; j++)
-    {
-        to_show.at<uchar>(i, j) = point_cloud_ptr->at(counter).z;
-        counter++;
-    }
-  }
-      cv::imshow("rec2", to_show);
-      cv::waitKey();*/
+    int counter = 0;
     for(int k = 0; k < clusters.size(); k++)
     {
-        cv::Mat res = cv::Mat::zeros(288+0, 384+0, CV_8U);
+        cv::Mat res = cv::Mat::zeros(img_rgb.rows, img_rgb.cols, CV_8U);
         //for(int i =0 ; i < point_cloud_ptr->points.size(); i++)
         for(int j =0 ; j < clusters[k].indices.size(); j++)
         {
